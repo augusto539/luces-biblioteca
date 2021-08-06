79a20667ba0f972fc71cf499a03833c2fa203137
augusto <augustoantonelli88@gmail.com>
2021-08-06 13:53:43 -0300

    togle upgraded

--- script.js
+++ script.js
@@ -16,4 +16,57 @@ function togle(id){
     {
         document.getElementById(id).className = "uncheck";
     }
+
+    if (id == 6 && elemet == 'uncheck'){
+        document.getElementById(0).className = "check";
+        document.getElementById(1).className = "check";
+        document.getElementById(2).className = "check";
+        document.getElementById(3).className = "check";
+        document.getElementById(4).className = "check";
+        document.getElementById(5).className = "check";
+
+        document.getElementById(7).className = "uncheck"; 
+        document.getElementById(8).className = "uncheck"; 
+    } if (id == 6 && elemet == 'check'){
+        document.getElementById(0).className = "uncheck";
+        document.getElementById(1).className = "uncheck";
+        document.getElementById(2).className = "uncheck";
+        document.getElementById(3).className = "uncheck";
+        document.getElementById(4).className = "uncheck";
+        document.getElementById(5).className = "uncheck"; 
+    }
+
+
+    if (id == 7 && elemet == 'uncheck'){
+        document.getElementById(0).className = "check";
+        document.getElementById(1).className = "check";
+        document.getElementById(2).className = "check";
+        document.getElementById(3).className = "check";
+
+        document.getElementById(4).className = "uncheck";
+        document.getElementById(5).className = "uncheck";
+        document.getElementById(6).className = "uncheck"; 
+        document.getElementById(8).className = "uncheck"; 
+    } if (id == 7 && elemet == 'check'){
+        document.getElementById(0).className = "uncheck";
+        document.getElementById(1).className = "uncheck";
+        document.getElementById(2).className = "uncheck";
+        document.getElementById(3).className = "uncheck";
+    }
+
+    if (id == 8 && elemet == 'uncheck'){
+        document.getElementById(4).className = "check";
+        document.getElementById(5).className = "check";
+
+        document.getElementById(0).className = "uncheck";
+        document.getElementById(1).className = "uncheck";
+        document.getElementById(2).className = "uncheck"; 
+        document.getElementById(3).className = "uncheck"; 
+
+        document.getElementById(6).className = "uncheck"; 
+        document.getElementById(7).className = "uncheck"; 
+    } if (id == 8 && elemet == 'check'){
+        document.getElementById(4).className = "uncheck";
+        document.getElementById(5).className = "uncheck";
+    }
 }
