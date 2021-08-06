0c63514450881fdf767c9118b8f409b51ed36689
augusto <augustoantonelli88@gmail.com>
2021-08-06 12:30:28 -0300

    new function

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+var music;
+var elemet;
+
 function playMusic(){
-    var music = new Audio('sound-14.mp3');
+    music = new Audio('sound-14.mp3');
     music.play();
+}
+
+function togle(id){
+    elemet = document.getElementById(id).className;
+
+    if (elemet == 'uncheck'){
+        document.getElementById(id).className = "check";
+    }
+    else
+    {
+        document.getElementById(id).className = "uncheck";
+    }
 }
