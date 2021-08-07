e95674ed36de2ebf383708346c9abe66f80e51f3
augusto <augustoantonelli88@gmail.com>
2021-08-07 12:47:53 -0300

    toc healed

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var music;
 var elemet;
 
+let buttoms = ['uncheck', 'uncheck', 'uncheck', 'uncheck', 'uncheck', 'uncheck', 'uncheck', 'uncheck', 'uncheck']
+
 function playMusic(){
     music = new Audio('sound-14.mp3');
     music.play();
@@ -10,63 +12,143 @@ function togle(id){
     elemet = document.getElementById(id).className;
 
     if (elemet == 'uncheck'){
-        document.getElementById(id).className = "check";
+        if (id < 6) {
+            buttoms[id] = 'check'
+        }
+        if (id == 6){
+            for (let index = 0; index < buttoms.length; index++) {
+                if (index < 7) {
+                    buttoms[index] = 'check'
+                } else {
+                    buttoms[index] = 'uncheck'
+                }
+            }
+        }
+        if (id == 7) {
+            for (let index = 0; index < buttoms.length; index++) {
+                if (index < 4 || index == 7) {
+                    buttoms[index] = 'check'
+                } else {
+                    buttoms[index] = 'uncheck'
+                }
+            }
+        }
+        if (id == 8) {
+            for (let index = 0; index < buttoms.length; index++) {
+                if (index == 4 || index == 5 || index == 8) {
+                    buttoms[index] = 'check'
+                } else {
+                    buttoms[index] = 'uncheck'
+                }
+            }
+        }
+        
+    } else {
+        if (id < 6) {
+            buttoms[id] = 'uncheck'
+        }  
+        if (id == 6) {
+            for (let index = 0; index < buttoms.length; index++) {
+                buttoms[index] = 'uncheck'  
+            }
+        }
+        if (id == 7) {
+            for (let index = 0; index < buttoms.length; index++) {
+                if (index < 4 || index == 7) {
+                    buttoms[index] = 'uncheck'
+                } 
+            }
+        }
+        if (id == 8) {
+            for (let index = 0; index < buttoms.length; index++) {
+                if (index == 4 || index == 5 || index == 8) {
+                    buttoms[index] = 'uncheck'
+                } 
+            }
+        }
     }
-    else
-    {
-        document.getElementById(id).className = "uncheck";
+
+    if (buttoms[0] == 'check' && buttoms[1] == 'check' && buttoms[2] == 'check' && buttoms[3] == 'check' && buttoms[4] == 'check' && buttoms[5] == 'check') {
+        buttoms[6] = 'check'
+ 
+    } else {
+        buttoms[6] = 'uncheck'
     }
 
-    if (id == 6 && elemet == 'uncheck'){
-        document.getElementById(0).className = "check";
-        document.getElementById(1).className = "check";
-        document.getElementById(2).className = "check";
-        document.getElementById(3).className = "check";
-        document.getElementById(4).className = "check";
-        document.getElementById(5).className = "check";
+    if (buttoms[0] == 'check' && buttoms[1] == 'check' && buttoms[2] == 'check' && buttoms[3] == 'check' && buttoms[4] != 'check' && buttoms[5] != 'check') {
+        buttoms[7] = 'check'
+    } else {
+        buttoms[7] = 'uncheck'
+    }
 
-        document.getElementById(7).className = "uncheck"; 
-        document.getElementById(8).className = "uncheck"; 
-    } if (id == 6 && elemet == 'check'){
-        document.getElementById(0).className = "uncheck";
-        document.getElementById(1).className = "uncheck";
-        document.getElementById(2).className = "uncheck";
-        document.getElementById(3).className = "uncheck";
-        document.getElementById(4).className = "uncheck";
-        document.getElementById(5).className = "uncheck"; 
+    if (buttoms[0] != 'check' && buttoms[1] != 'check' && buttoms[2] != 'check' && buttoms[3] != 'check' && buttoms[4] == 'check' && buttoms[5] == 'check') {
+        buttoms[8] = 'check'
+    } else {
+        buttoms[8] = 'uncheck'
+    }
+    
+
+
+    for (let index = 0; index < buttoms.length; index++) {
+        document.getElementById(index).className = buttoms[index];   
     }
 
 
-    if (id == 7 && elemet == 'uncheck'){
-        document.getElementById(0).className = "check";
-        document.getElementById(1).className = "check";
-        document.getElementById(2).className = "check";
-        document.getElementById(3).className = "check";
+}
 
-        document.getElementById(4).className = "uncheck";
-        document.getElementById(5).className = "uncheck";
-        document.getElementById(6).className = "uncheck"; 
-        document.getElementById(8).className = "uncheck"; 
-    } if (id == 7 && elemet == 'check'){
-        document.getElementById(0).className = "uncheck";
-        document.getElementById(1).className = "uncheck";
-        document.getElementById(2).className = "uncheck";
-        document.getElementById(3).className = "uncheck";
-    }
 
-    if (id == 8 && elemet == 'uncheck'){
-        document.getElementById(4).className = "check";
-        document.getElementById(5).className = "check";
 
-        document.getElementById(0).className = "uncheck";
-        document.getElementById(1).className = "uncheck";
-        document.getElementById(2).className = "uncheck"; 
-        document.getElementById(3).className = "uncheck"; 
+/*
 
-        document.getElementById(6).className = "uncheck"; 
-        document.getElementById(7).className = "uncheck"; 
-    } if (id == 8 && elemet == 'check'){
-        document.getElementById(4).className = "uncheck";
-        document.getElementById(5).className = "uncheck";
+
+
+if (id == 6){
+    for (let index = 0; index < buttoms.length; index++) {
+        if (index < 7) {
+            buttoms[index] = 'check'
+        } else {
+            buttoms[index] = 'uncheck'
+        }
     }
 }
+if (id == 7) {
+    for (let index = 0; index < buttoms.length; index++) {
+        if (index < 4 || index == 7) {
+            buttoms[index] = 'check'
+        } else {
+            buttoms[index] = 'uncheck'
+        }
+    }
+}
+if (id == 8) {
+    for (let index = 0; index < buttoms.length; index++) {
+        if (index == 4 || index == 5 || index == 8) {
+            buttoms[index] = 'check'
+        } else {
+            buttoms[index] = 'uncheck'
+        }
+    }
+}
+
+
+if (id == 6) {
+    for (let index = 0; index < buttoms.length; index++) {
+        buttoms[index] = 'uncheck'  
+    }
+}
+if (id == 7) {
+    for (let index = 0; index < buttoms.length; index++) {
+        if (index < 4 || index == 7) {
+            buttoms[index] = 'uncheck'
+        } 
+    }
+}
+if (id == 8) {
+    for (let index = 0; index < buttoms.length; index++) {
+        if (index == 4 || index == 5 || index == 8) {
+            buttoms[index] = 'uncheck'
+        } 
+    }
+}
+
+*/
